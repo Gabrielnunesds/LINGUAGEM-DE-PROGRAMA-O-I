979d311253570d874b5636b43760a5629af06bda
Gabriel Nunes <47859373+Gabrielnunesds@users.noreply.github.com>
2023-04-28 18:50:26 -0300

    Add files via upload

---
+++ Aula01_HelloWorld/src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+ // psvm - Atalho Metodo Main
+    public static void main(String[] args) {
+ // sout - Atalho Metodo Print
+        System.out.println("Hello world");
+
+    }
+}
+
